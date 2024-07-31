dc760c7bb76306f339ae0168e865255a0de7aca9
Erlangga Ishak <134130177+errtod12@users.noreply.github.com>
2024-07-31 20:39:09 +0700

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,34 @@
+// Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Pastikan untuk menambahkan CSS untuk styling
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <Link to="/">Absensi Wajah</Link>
+            </div>
+            <ul className="navbar-links">
+                <li>
+                    <Link to="/">Beranda</Link>
+                </li>
+                <li>
+                    <Link to="/about">Tentang</Link>
+                </li>
+                <li>
+                    <Link to="/features">Fitur</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/register">Daftar</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
 
