ccf1d1b7362d11555342c1252754e5fe01f1a174
Erlangga Ishak <134130177+errtod12@users.noreply.github.com>
2024-07-31 20:44:52 +0700

    Update NotFoundPage.js

--- frontend/src/pages/NotFoundPage.js
+++ frontend/src/pages/NotFoundPage.js
@@ -1,1 +1,19 @@
+// NotFoundPage.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFoundPage.css'; // Pastikan untuk menambahkan styling sesuai kebutuhan
+
+const NotFoundPage = () => {
+    return (
+        <div className="not-found">
+            <h1>404 - Halaman Tidak Ditemukan</h1>
+            <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
+            <Link to="/" className="home-link">
+                Kembali ke Beranda
+            </Link>
+        </div>
+    );
+};
+
+export default NotFoundPage;
 
