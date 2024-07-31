e4050485bbde4378f44e2ffc8aa97660c7dcf328
Erlangga Ishak <134130177+errtod12@users.noreply.github.com>
2024-07-31 20:53:10 +0700

    Update userReducers.js

--- frontend/src/store/reducers/userReducers.js
+++ frontend/src/store/reducers/userReducers.js
@@ -1,1 +1,44 @@
+// userReducers.js
+const initialState = {
+  users: [],
+  loading: false,
+  error: null,
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_USERS_REQUEST':
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case 'FETCH_USERS_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        users: action.payload,
+      };
+    case 'FETCH_USERS_FAILURE':
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case 'ADD_USER':
+      return {
+        ...state,
+        users: [...state.users, action.payload],
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
 
