a67b2ba8bc0f442cd8a18bc8c4abff5cfcd2233c
Erlangga Ishak <134130177+errtod12@users.noreply.github.com>
2024-07-31 20:21:29 +0700

    Update Face.js

--- backend/models/Face.js
+++ backend/models/Face.js
@@ -1,1 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const faceapi = require('face-api.js');
+const canvas = require('canvas');
+const fs = require('fs');
+
+// Load model wajah
+async function loadModels() {
+    await faceapi.nets.tinyFaceDetector.loadFromDisk('./weights/');
+    await faceapi.nets.faceLandmark68Net.loadFromDisk('./weights/');
+    await faceapi.nets.faceRecognitionNet.loadFromDisk('./weights/');
+}
+
+loadModels();
+
+router.post('/attendance', async (req, res) => {
+    const { imagePath } = req.body;
+
+    // Load gambar
+    const img = await canvas.loadImage(imagePath);
+    const detections = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors();
+
+    if (detections.length > 0) {
+        // Simpan atau update data absensi
+        const user = new User({
+            name: 'Nama Pengguna',
+            attendanceDate: new Date(),
+            imagePath: imagePath
+        });
+
+        await user.save();
+        res.status(200).send('Absensi berhasil dicatat');
+    } else {
+        res.status(400).send('Wajah tidak terdeteksi');
+    }
+});
+
+module.exports = router;
 
