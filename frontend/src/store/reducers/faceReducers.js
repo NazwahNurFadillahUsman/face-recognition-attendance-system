b4b585a90aa49085504db5067366b85921f3e4b7
Erlangga Ishak <134130177+errtod12@users.noreply.github.com>
2024-07-31 20:52:19 +0700

    Update faceReducers.js

--- frontend/src/store/reducers/faceReducers.js
+++ frontend/src/store/reducers/faceReducers.js
@@ -1,1 +1,65 @@
+// faceReducers.js
+const initialState = {
+    faces: [],
+    loading: false,
+    error: null,
+};
+
+// Action Types
+const ADD_FACE = 'ADD_FACE';
+const REMOVE_FACE = 'REMOVE_FACE';
+const SET_LOADING = 'SET_LOADING';
+const SET_ERROR = 'SET_ERROR';
+
+// Reducer Function
+const faceReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_FACE:
+            return {
+                ...state,
+                faces: [...state.faces, action.payload],
+            };
+        case REMOVE_FACE:
+            return {
+                ...state,
+                faces: state.faces.filter(face => face.id !== action.payload.id),
+            };
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: action.payload,
+            };
+        case SET_ERROR:
+            return {
+                ...state,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+// Action Creators
+export const addFace = (face) => ({
+    type: ADD_FACE,
+    payload: face,
+});
+
+export const removeFace = (id) => ({
+    type: REMOVE_FACE,
+    payload: { id },
+});
+
+export const setLoading = (isLoading) => ({
+    type: SET_LOADING,
+    payload: isLoading,
+});
+
+export const setError = (error) => ({
+    type: SET_ERROR,
+    payload: error,
+});
+
+// Export the reducer
+export default faceReducer;
 
