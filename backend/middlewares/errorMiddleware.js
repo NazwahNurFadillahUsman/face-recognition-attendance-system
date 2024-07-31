5191c3ca7c9e1571f5cee7234175cca55507cddc
Erlangga Ishak <134130177+errtod12@users.noreply.github.com>
2024-07-31 20:16:27 +0700

    Update errorMiddleware.js

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,1 +1,19 @@
+// errorMiddleware.js
+
+// Middleware untuk menangani kesalahan
+function errorMiddleware(err, req, res, next) {
+  console.error(err.stack);
+
+  // Tentukan status kode kesalahan
+  const statusCode = err.statusCode || 500;
+
+  // Tanggapan kesalahan umum
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || 'Internal Server Error',
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+}
+
+module.exports = errorMiddleware;
 
