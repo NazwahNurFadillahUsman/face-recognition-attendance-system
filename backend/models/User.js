6c2b93c0ee05a4365d48253e2cc60e0cb5ab97be
Erlangga Ishak <134130177+errtod12@users.noreply.github.com>
2024-07-31 20:22:14 +0700

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,37 @@
+const mongoose = require('mongoose');
+
+// Membuat schema untuk User
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    faceData: {
+        type: String, // Simpan data wajah dalam format yang sesuai (base64, URL, dll.)
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Membuat model User
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
 
